394a701c2da71d1217439bcff3eda5124a3e3e25
Amos Chanza Sikazwe <40888297+AChanzaS@users.noreply.github.com>
2022-03-21 04:03:11 +0200

    Made two funcions of the project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
+var playerSelection;
+var computerSelection;
+
+//This is the function of the computer play
 function computerPlay() {
+
+    var selection;
+    var cases = Math.floor(Math.random() * (4-0+1) + 0);
+    
+    switch (cases){
+        case 1:
+            selection = "rock";
+            break;
+
+        case 2:
+            selection = "paper";
+            break;
+
+        case 3:
+            selection = "scissors";
+            break;
+            
+    }
+
+    return computerSelection = selection;
+
+}
+
+// The function that takes the results from the computer and the Human plays
+function playerPlay (playerSelection, computerSelection) {
+
+    var selection = playerSelection.toLowerCase();
+    return playerSelection = selection;
     
 }
