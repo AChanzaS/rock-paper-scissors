0752258d2d66ba3bad2b1810430bde3314f63b92
Amos Chanza Sikazwe <40888297+AChanzaS@users.noreply.github.com>
2022-03-26 10:53:14 +0200

    Added the functionalities for the enhancement of the game

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var playerSelection;
+var humanSelection;
 var computerSelection;
 
 //This is the function of the computer play
@@ -27,9 +27,67 @@ function computerPlay() {
 }
 
 // The function that takes the results from the computer and the Human plays
-function playerPlay (playerSelection, computerSelection) {
+function humanPlay (humanSelection) {
 
-    var selection = playerSelection.toLowerCase();
-    return playerSelection = selection;
+    var selection = humanSelection.toLowerCase();
+    return humanSelection = selection;
     
+}
+
+function humanWins(){
+    console.log("Human wins");
+}
+
+function computerWins(){
+    console.log("Computer wins");
+}
+function draw(){
+    console.log("draw play!");
+}
+
+function compare (humanSelection, computerSelection){
+    if (humanSelection == "rock" &&  computerSelection == "rock"){
+        console.log();
+        draw();
+    }
+    else 
+    if (humanSelection == "rock" &&  computerSelection == "paper"){
+        console.log();
+        computerWins();
+    }
+    else 
+    if (humanSelection == "rock" &&  computerSelection == "scissors"){
+        console.log();
+        humanWins();
+    }
+    else 
+    if (humanSelection == "paper" &&  computerSelection == "rock"){
+        console.log();
+        humanWins();
+    }
+    else 
+    if (humanSelection == "paper" &&  computerSelection == "paper"){
+        console.log();
+        draw();
+    }
+    else 
+    if (humanSelection == "paper" &&  computerSelection == "scissors"){
+        console.log();
+        computerWins();
+    }
+    else 
+    if (humanSelection == "scissors" &&  computerSelection == "rock"){
+        console.log();
+        computerWins();
+    }
+    else 
+    if (humanSelection == "scissors" &&  computerSelection == "paper"){
+        console.log();
+        humanWins();
+    }
+    else 
+    if (humanSelection == "scissors" &&  computerSelection == "scissors"){
+        console.log();
+        draw();
+    }
 }
